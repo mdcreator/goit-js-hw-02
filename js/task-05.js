2c755fd977b85990d2f3463aa1de1ba0f9a594f5
Marina Domashenko <creator.marina@gmail.com>
2020-08-06 15:47:56 +0300

    Added task-04.js

---
+++ js/task-05.js
@@ -0,0 +1,21 @@
+// Функция предикат
+// Функция предикат возвращает true или false
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.
+// Функция проверяет ее на содержание слов spam и sale.Если нашли запрещенное слово то функция возвращает true,
+// если запрещенных слов нет функция возвращает false.Слова в строке могут быть в произвольном регистре.
+
+function checkForSpam(str) {
+  'use strict';
+
+  let message = str.toLowerCase();
+  return message.includes('spam') || message.includes('sale');
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
